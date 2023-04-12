66db9e3c39a0f37e6d8cd82783dce8dd38bd2bb7
doctored11 <edit.shultz@yandex.ru>
2023-04-12 19:10:46 +0300

    add tap + tap ani

--- eggtapper/app/src/main/java/nth11/game/eggtapper/MainActivity.java
+++ eggtapper/app/src/main/java/nth11/game/eggtapper/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //отключаем темную тему от греха подальше ( возможно временно)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+        ImageView egg = findViewById(R.id.egg);
+        egg.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    view.animate().scaleX(0.97f).scaleY(0.95f).setDuration((long) 0);
+
+                } else if ((motionEvent.getAction() == MotionEvent.ACTION_UP)){
+                    view.animate().scaleX(1f).scaleY(1f).setDuration((long) 0.1);
+                }
+
+                return true;
+            }
+        });
+
+
     }
 }
