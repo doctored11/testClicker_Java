385067256cb45382f683dc081652bfb95a4c5147
doctored11 <edit.shultz@yandex.ru>
2023-04-15 00:00:06 +0300

    some refactor

--- eggtapper/app/src/main/java/nth11/game/eggtapper/MainActivity.java
+++ eggtapper/app/src/main/java/nth11/game/eggtapper/MainActivity.java
@@ -3,6 +3,7 @@ package nth11.game.eggtapper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -26,78 +27,86 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
     ViewModel model;
     FragmentTransaction ft;
+    private Fragment shopFragment;
+    private FragmentManager fragmentManager;
 
+    private TextView textCount;
+    private TextView txtMoney;
+    private ImageView egg;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("!!!");
         setContentView(R.layout.activity_main);
+
+        model = new ViewModelProvider(this).get(ViewModel.class);
+
+        fragmentManager = getSupportFragmentManager();
+        shopFragment = model.getShopFragment();
+
         //отключаем темную тему от греха подальше ( возможно временно)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         //создание этой вонючей ViewModel
-        model = new ViewModelProvider(this).get(ViewModel.class);
 
-        TextView text_count = findViewById(R.id.text_count);
-        TextView txt_money = findViewById(R.id.money_txt);
+
+        textCount = findViewById(R.id.text_count);
+        txtMoney = findViewById(R.id.money_txt);
 
         shopBtn = findViewById(R.id.shop_btn);
         frameShop = findViewById(R.id.fragment_shop);
 
         progressBar = findViewById(R.id.progress);
 
-        ImageView egg = findViewById(R.id.egg);
+        egg = findViewById(R.id.egg);
 
         //Подписка на изменения uiState из viewModel
         model.getUiState().observe(this, uiState -> {
 
             // update UI
-            text_count.setText(uiState.getStrenght() + " ");
+            textCount.setText(uiState.getStrenght() + " ");
             progressBar.setProgress(uiState.getStrenght());
-            txt_money.setText(getString(R.string.txt_money) + " " + uiState.getMoney());
+            txtMoney.setText(getString(R.string.txt_money) + " " + uiState.getMoney());
 
-            setFragment(model.getShopFragment(), uiState.getShopActive());
+            setFragment( uiState.getShopActive());
 
 
         });
 
-        shopBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean fl = model.getUiState().getValue().getShopActive();
-                model.onShopClick(!fl);
-
-            }
+        shopBtn.setOnClickListener(view -> {
+            boolean fl = model.getUiState().getValue().getShopActive();
+            model.onShopClick(!fl);
         });
 
 
 
 
-        egg.setOnTouchListener(new View.OnTouchListener() {
-            @SuppressLint({"ClickableViewAccessibility", "SetTextI18n"})
-            @Override
-            public boolean onTouch(@SuppressLint("ClickableViewAccessibility") View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    view.animate().scaleX(0.97f).scaleY(0.95f).setDuration((long) 0);
-                    model.onTap();
-                } else if ((motionEvent.getAction() == MotionEvent.ACTION_UP)) {
-                    view.animate().scaleX(1f).scaleY(1f).setDuration((long) 0.1);
-                    return false;
-                }
-                return true;
+        egg.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                animateEgg(view, true);
+                model.onTap();
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                animateEgg(view, false);
+                return false;
             }
+            return true;
         });
     }
 
     //это логика только отображения - оставляем во View?
-    private void setFragment(Fragment fragment, Boolean flag) {
-        ft = getSupportFragmentManager().beginTransaction();
-        ft.remove(fragment);
+    private void setFragment(boolean flag) {
+         ft = fragmentManager.beginTransaction();
         if (flag) {
-            ft.add(R.id.fragment_shop, fragment);
+            ft.replace(R.id.fragment_shop, shopFragment);
         } else {
-            ft.remove(fragment);
+            ft.remove(shopFragment);
         }
         ft.commit();
     }
+    private void animateEgg(View view, boolean isDown) {
+        float scaleX = isDown ? 0.97f : 1f;
+        float scaleY = isDown ? 0.95f : 1f;
+        view.animate().scaleX(scaleX).scaleY(scaleY).setDuration(isDown ? 0 : (long) 0.1);
+    }
+
+
 }
--- eggtapper/app/src/main/java/nth11/game/eggtapper/ShopFragment.java
+++ eggtapper/app/src/main/java/nth11/game/eggtapper/ShopFragment.java
@@ -1,21 +1,18 @@
 package nth11.game.eggtapper;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
 public class ShopFragment extends Fragment {
-    ViewModel model;
-    private Player player;
+
+    private ViewModel model;
 
     public ShopFragment(ViewModel model) {
         this.model = model;
@@ -25,53 +22,28 @@ public class ShopFragment extends Fragment {
         // Конструктор без аргументов
     }
 
-    public void setModel(ViewModel model){
+    public void setModel(ViewModel model) {
         this.model = model;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//tool up
+
         View view = inflater.inflate(R.layout.fragment_shop, container, false);
+
         LinearLayout toolUp = view.findViewById(R.id.toolUp);
         TextView coastTool = view.findViewById(R.id.text_toolUp_coast);
-        //incubator Up
-
+        coastTool.setText(String.format("$ = %d", model.getUiState().getValue().getToolUpCoast()));
 
         LinearLayout incUp = view.findViewById(R.id.incubator_first_up);
         TextView coastInc = view.findViewById(R.id.text_incubator_first_coast);
+        coastInc.setText(String.format("$ = %d", model.getUiState().getValue().getIncubatorUpCoast()));
 
-
-        coastTool.setText("$ = " + model.getUiState().getValue().getToolUpCoast());
-        coastInc.setText("$ = "+ model.getUiState().getValue().getIncubatorUpCoast());
-        toolUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (model != null) {
-                    model.onToolUp();
-                }
-
-            }
-        });
-        incUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (model != null) {
-                   model.onIncUp();
-                }
-
-            }
-        });
-//
+        toolUp.setOnClickListener(v -> model.onToolUp());
+        incUp.setOnClickListener(v -> model.onIncUp());
 
         return view;
     }
-
-
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
 
 }
